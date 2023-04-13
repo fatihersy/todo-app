e5f917bfec2ffcc9a89f3628203a5ea80cfb096b
fatihersy <ersy.usl@gmail.com>
2023-04-13 21:38:12 +0300

    Removing environments folder from the gitignore for future check

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment  = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDryXNYRlUIKg4j4g0cDt9xcFE7OzK4l5A",
+    authDomain: "todo-app-9faa9.firebaseapp.com",
+    databaseURL: "https://todo-app-9faa9-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "todo-app-9faa9",
+    storageBucket: "todo-app-9faa9.appspot.com",
+    messagingSenderId: "798657879208",
+    appId: "1:798657879208:web:787564a2fcec77759ff69d"
+  }
+};
